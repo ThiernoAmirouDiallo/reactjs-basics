448d3a25d564d762ffd18c465be84bc56d674a44
ThiernoAmirouDIALLO <thiernoamirou.diallo2@orange-sonatel.com>
2019-02-13 13:03:24 +0000

    react-redux

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,18 +1,21 @@
 import React from "react";
 
-export const User = (props) => {
+export const Main = (props) => {
     return (
         <div>
             <div className="row">
                 <div className="col-xs-12">
-                    <h1>The User Page</h1>
+                    <h1>The Main Page</h1>
                 </div>
             </div>
             <div className="row">
                 <div className="col-xs-12">
-                    <p>User Name: {props.username}</p>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => props.changeUsername('Anna')}>Change the Username
+                    </button>
                 </div>
             </div>
         </div>
     );
-}
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,95 +1,12 @@
-/* import React from "react";
-import { render } from "react-dom";
-//import {Router, Route, browserHistory } from "react-router"
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {render} from "react-dom";
+import React from "react";
+import {Provider} from "react-redux";
 
+import App from "./containers/App";
+import store from "./store";
 
-import Root from "./components/Root";
-import Home from "./components/Home";
-import User from "./components/User";
-import Header from "./components/Header";
-
-class App extends React.Component
-{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return (
-            <Router>
-
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-10 col-sm-offset-1">
-                            <Header />
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-sm-10 col-sm-offset-1">
-                            <Route exact path={"/"} component={Home}/>
-                            <Route path={"/home"} component={Home}/>
-                            <Route path={"/user/:id"} component={User}/>
-                        </div>
-                    </div>
-                </div>
-
-
-            </Router>
-        );
-    }
-}
-
-render(<App/>,window.document.getElementById("app"))
-
-*/
-
-import { createStore } from "redux";
-
-const initialState= {
-    result:1,
-    lastValues : []
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type){
-      case  "ADD":
-          state = {
-              ...state,
-              result: state.result + action.payload,
-              lastValues: [...state.lastValues, action.payload]
-          }
-          break;
-
-      case "SUBTRACT":
-          state = {
-              ...state,
-              result: state.result - action.payload,
-              lastValues: [...state.lastValues, action.payload]
-          }
-          break;
-  }
-  return state;
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => {
-    console.log("Store updated :", store.getState());
-});
-
-store.dispatch({
-    type: "ADD",
-    payload:100
-});
-
-store.dispatch({
-    type: "ADD",
-    payload:22
-});
-
-store.dispatch({
-    type: "SUBTRACT",
-    payload:80
-});
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    window.document.getElementById('app'));
